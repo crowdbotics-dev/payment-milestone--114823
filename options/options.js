e8acab62e9d05cfb735a1a11712aade017bbdc2d
Crowdbotics <engineering@crowdbotics.com>
2023-09-07 11:52:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "payment-milestone--114823",
+  url: "https://payment-milestone--114823.botics.co",
+  api: "https://payment-milestone--114823.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
